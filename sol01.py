cfe7754ce479bf9b5ee5c7e9dedb9591fd823a43
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2021-09-24 14:50:14 +0900

    baekjoon_1436
    - 0부터 숫자를 1씩 증가하여 666이 포함되어 있는지 체크
    - 포함되어 있는 숫자와 비교할 차례면 카운트 1 증가
    - 카운트가 초기 입력값 n과 일치할 경우 while문 중단

--- sol01.py
+++ sol01.py
@@ -5,31 +5,45 @@ Created on Wed Jul 14 16:56:54 2021
 @author: s_dlwlstks96
 """    
 
-n,m = map(int, input().split())
+#1436
 
-board = [] #입력 받는 체스판
-count = [] #뒤집어야할 갯수 정보를 담기 위한 리스트
+n = int(input())
 
-for i in range(n):
-    board.append(input())
+find = True #while문 제어하기 위한 boolean
+num = 0 #0부터 시작하여 비교할 숫자를 계속 제공하기 위함
+cnt = 0 #666이 들어간 숫자가 몇번째인지 카운트하기 위함
 
-for i in range(n-7):
-    for j in range(m-7): #i, j로 전체 체스판에서 시작점을 잡기 위함
-        start_white_check = 0 #'W'로 시작할 경우 바뀐 칸의 갯수 카운트
-        start_black_check = 0 #'B'로 시작할 경우 바뀐 칸의 갯수 카운트
-        for a in range(i, i+8):
-            for b in range(j, j+8): #시작점에서부터 8x8 크기의 체스칸 확인하게 해줌
-                #a, b 합이 짝수면 시작점의 색과 같아야하고 홀수의 경우 달라야 한다
-                if (a+b) % 2 == 0:
-                    if board[a][b] != 'W': #시작점이 'W'라는 뜻
-                        start_white_check += 1 #'W'로 시작했을 경우의 변수 카운트
-                    if board[a][b] != 'B': #시작점이 'B'라는 뜻
-                        start_black_check += 1 #'B'로 시작했을 경우의 변수 카운트
-                else:
-                    if board[a][b] != 'B': #시작점이 'W'라는 뜻
-                        start_white_check += 1 #'W'로 시작했을 경우의 변수 카운트
-                    if board[a][b] != 'W': #시작점이 'B'라는 뜻
-                        start_black_check += 1 #'B'로 시작했을 경우의 변수 카운트
-        count.append(min(start_white_check, start_black_check))
+while(find):
+    num = str(num)
+    #666이 현재 비교 숫자에 들어가 있으면 카운트 1 증가
+    if '666' in num:
+        cnt += 1
+    #카운트가 입력한 n의 값과 일치하면 while문 중단
+    if cnt == n:
+        find = False
+    #일치하지 않으면 비교 숫자 1 증가
+    else:
+        num = int(num) + 1
         
-print(min(count))                 
+print(num)
+
+# 666
+# 1666
+# 2666
+# 3666
+# 4666
+# 5666
+# .
+# .
+# 9666
+# 10666
+# 11666
+# 12666
+# .
+# .
+# 19666
+# 20666
+# 21666
+# .
+# .
+# 16661
