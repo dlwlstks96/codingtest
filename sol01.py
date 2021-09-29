87222f66112cbfe4d33a2ebdba4a992c2f6d420e
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2021-09-29 20:02:13 +0900

    baekjoon_1427

--- sol01.py
+++ sol01.py
@@ -5,16 +5,18 @@ Created on Wed Jul 14 16:56:54 2021
 @author: s_dlwlstks96
 """    
 
-#2750
+#1427
 
-n = int(input())
-res = []
+n = input()
+arr = []
 
-for i in range(n):
-    k = int(input())
-    res.append(k)
+for i in range(len(n)):
+    arr.append(int(n[i]))
+
+arr.sort(reverse=True)
+
+res = ""
+for i in range(len(arr)):
+    res += str(arr[i])
     
-res.sort()
-
-for i in range(n):
-    print(res[i])
+print(res)
