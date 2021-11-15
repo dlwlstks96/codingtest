9138e928df686358b57bd966b9524dcc76f0cb3a
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2021-11-15 15:48:56 +0900

    baekjoon_11650

--- sol01.py
+++ sol01.py
@@ -5,18 +5,21 @@ Created on Wed Jul 14 16:56:54 2021
 @author: s_dlwlstks96
 """    
 
-#1427
+#11650
 
-n = input()
+import sys
+
+#stdin.readline은 한번에 읽어와 버퍼에 저장하기에
+#하나씩 입력할 때마다 데이터를 버퍼에 보관하는 input()보다
+#처리 속도가 빠르다
+n = int(sys.stdin.readline())
+
 arr = []
-
-for i in range(len(n)):
-    arr.append(int(n[i]))
-
-arr.sort(reverse=True)
-
-res = ""
-for i in range(len(arr)):
-    res += str(arr[i])
+for i in range(n):
+    x, y = map(int, input().split())
+    arr.append([x, y])
     
-print(res)
+s_arr = sorted(arr)
+
+for i in range(n):
+    print(s_arr[i][0], s_arr[i][1])
