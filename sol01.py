0dc0336d3b240d58e82c5530baf14337f8da39af
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-01-12 16:23:31 +0900

    프로그래머스_힙_이중우선순위큐

--- sol01.py
+++ sol01.py
@@ -1,51 +1,32 @@
 # -*- coding: utf-8 -*-
 
-#프로그래머스_힙_디스크 컨트롤러
+#프로그래머스_힙_이중우선순위큐
 
-import heapq as hq
+operations = ["I 7","I 5","I -5","D -1"]
+#operations = ["I 16","D 1"]
 
-jobs = [[0, 3], [1, 9], [2, 6]]
-
-def solution(jobs):
-    answer = 0
+def solution(operations):
+    que = []
     
-    jobs_len = len(jobs)
-    
-    hq.heapify(jobs) #힙 이용해 jobs 정렬
-    
-    time = 0
-    now_task_finish_time = 0 #현재 작업의 종료 예정 시간
-    now_task = [0, 0] #현재 진행중인 작업
-    wait_task = [] #대기중인 작업 목록
-    task_time_list = [] #각 요청부터 종료까지의 시간 목록
-    
-    while(True):
-        
-        if len(jobs) > 0 and min(jobs)[0] <= time: #지금 시간에 요청 들어온 작업이 있는 경우
-                #min으로 받아오는 이유는 현재 진행과 대기 중인 작업이 없을 경우엔
-                #가장 요청이 빠른 작업을 받아 오기 위함
-                tmp = min(jobs)
-                idx = jobs.index(tmp)
-                jobs.pop(idx)
-                tmp.reverse()#wait_task를 힙으로 정렬해 최소값을 가져오기 위해 순서 바꿈
-                hq.heappush(wait_task, tmp)
-        elif len(jobs) == 0 and len(wait_task) == 0:
-            break
-
+    for ele in operations:
+        if ele[0] == 'I': #원소 삽입
+            num = int(ele[2:])
+            que.append(num)
+        elif ele[0] == 'D': #원소 삭제
+            if ele[2] == '1' and len(que) > 0: #최대값 삭제
+                max_idx = que.index(max(que))
+                que.pop(max_idx)
+            elif ele[2] == '-' and len(que) > 0: #최소값 삭제
+                min_idx = que.index(min(que))
+                que.pop(min_idx)
                 
-        if time >= now_task_finish_time: #진행중이던 작업이 끝난 경우
-            if len(wait_task) >= 1: #대기중인 작업 1개 이상
-                now_task = hq.heappop(wait_task) #소요시간 최소 작업을 가져옴
-                now_task_finish_time = now_task[0] + time
-                task_time_list.append(now_task_finish_time - now_task[1])
-                time += 1
-            elif len(wait_task) < 1: #대기중인 작업 X
-                time += 1
-                       
-        else: #아직 작업중일때
-            time = time + 1
-
-    answer = int(sum(task_time_list) / jobs_len)
+    if len(que) == 0:
+        answer = [0,0]
+    elif len(que) > 1: #min, max를 쓸 땐 해당 리스트에 대한 길이를 확인해줘야함
+        min_num = min(que)
+        max_num = max(que)
+        answer = [max_num, min_num]
+    
     return answer
 
-solution(jobs)
+solution(operations)
