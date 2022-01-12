3245a5e2c150429ebb79ff2d86a86429f8053628
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-01-12 15:31:46 +0900

    프로그래머스_힙_디스크 컨트롤러

--- sol01.py
+++ sol01.py
@@ -1,17 +1,51 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 14 16:56:54 2021
 
-@author: s_dlwlstks96
-"""    
+#프로그래머스_힙_디스크 컨트롤러
 
-#11726
+import heapq as hq
 
-s = [0,1,2]
+jobs = [[0, 3], [1, 9], [2, 6]]
 
-for i in range(3, 1001):
-    s.append(s[i-2] + s[i-1]) #n의 방법의 수는 n-2 + n-1
+def solution(jobs):
+    answer = 0
     
-n = int(input())
+    jobs_len = len(jobs)
+    
+    hq.heapify(jobs) #힙 이용해 jobs 정렬
+    
+    time = 0
+    now_task_finish_time = 0 #현재 작업의 종료 예정 시간
+    now_task = [0, 0] #현재 진행중인 작업
+    wait_task = [] #대기중인 작업 목록
+    task_time_list = [] #각 요청부터 종료까지의 시간 목록
+    
+    while(True):
+        
+        if len(jobs) > 0 and min(jobs)[0] <= time: #지금 시간에 요청 들어온 작업이 있는 경우
+                #min으로 받아오는 이유는 현재 진행과 대기 중인 작업이 없을 경우엔
+                #가장 요청이 빠른 작업을 받아 오기 위함
+                tmp = min(jobs)
+                idx = jobs.index(tmp)
+                jobs.pop(idx)
+                tmp.reverse()#wait_task를 힙으로 정렬해 최소값을 가져오기 위해 순서 바꿈
+                hq.heappush(wait_task, tmp)
+        elif len(jobs) == 0 and len(wait_task) == 0:
+            break
 
-print(s[n] % 10007) #n의 방법의 수를 리스트에서 골라 10007로 나누기
+                
+        if time >= now_task_finish_time: #진행중이던 작업이 끝난 경우
+            if len(wait_task) >= 1: #대기중인 작업 1개 이상
+                now_task = hq.heappop(wait_task) #소요시간 최소 작업을 가져옴
+                now_task_finish_time = now_task[0] + time
+                task_time_list.append(now_task_finish_time - now_task[1])
+                time += 1
+            elif len(wait_task) < 1: #대기중인 작업 X
+                time += 1
+                       
+        else: #아직 작업중일때
+            time = time + 1
+
+    answer = int(sum(task_time_list) / jobs_len)
+    return answer
+
+solution(jobs)
