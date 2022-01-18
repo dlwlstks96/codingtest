ad14e750c5aad6f909d3c1debbd9d796930defc4
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-01-18 16:03:07 +0900

    프로그래머스_정렬_H-index

--- sol01.py
+++ sol01.py
@@ -1,26 +1,21 @@
 # -*- coding: utf-8 -*-
 
-#프로그래머스_정렬_가장큰수
+#프로그래머스_정렬_H-index
 
-#numbers = [0, 0, 0]
-#numbers = [989, 98]
-numbers = [3, 34, 32]
-#numbers = [3, 30, 34, 5, 9, 999]
+#citations = [3, 0, 6, 1, 5]
+citations = [9, 7, 6, 2, 1]
 
-def solution(numbers):
-    answer = ''
+def solution(citations):
+    answer = 0
     
-    #numbers의 요소들을 문자열로 변경
-    numbers = list(map(str, numbers))
+    citations.sort(reverse = True) #내림차순 정렬
     
-    #sort(reverse)로 내림차순 정렬
-    #x*3의 이유는 numbers의 원소가 0 이상 1000이하이기에
-    numbers.sort(key = lambda x: x*1, reverse = True)
-    #66 22 1010
+    for i in range(len(citations)):
+        #리스트의 i번째 값이 i+1 이상일 경우
+        #요구 조건에 부합
+        if citations[i] >= i+1:
+            answer = i+1
     
-    print(numbers)
-    
-    #[0,0]의 경우 00이 나오기에 int로 변경 후 다시 str로
-    return str(int(''.join(numbers)))
+    return answer
 
-solution(numbers)
+solution(citations)
