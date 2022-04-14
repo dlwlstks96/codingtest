f09e66e843c98ef6776f06d508c2867315380f02
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-04-14 13:55:55 +0900

    프로그래머스-2019 카카오 블라인드-길찾기 게임-정답 열람-주석 추가

--- 프로그래머스/2019 카카오 블라인드_길찾기 게임.py
+++ 프로그래머스/2019 카카오 블라인드_길찾기 게임.py
@@ -2,7 +2,7 @@
 
 import sys
 
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**6) #재귀 호출 제한 해제
 
 class Node:
     def __init__(self, key, x):
@@ -16,19 +16,19 @@ class Tree:
         
     def insert(self, key, x):
         cur_node = self.head
-        while True:
+        while True: #루트 노드부터 단말 노드 위치까지 내려감
             if cur_node.x > x: #left down
-                if cur_node.left == None: 
+                if cur_node.left == None: #None일때만 노드 insert
                     cur_node.left = Node(key, x)
                     break
-                else: cur_node = cur_node.left
+                else: cur_node = cur_node.left #cur_node 갱신
             elif cur_node.x < x: #right down
                 if cur_node.right==None: 
                     cur_node.right = Node(key, x)
                     break
                 else: cur_node = cur_node.right
 
-    #전위 순회
+    #전위 순회 / 부모 -> 왼쪽 자식 -> 오른쪽 자식
     def preorder(self):
         res = []  
         def order(node):
@@ -39,7 +39,7 @@ class Tree:
         order(self.head)
         return res
         
-    #후위 순회
+    #후위 순회 / 왼쪽 자식 -> 오른쪽 자식 -> 부모
     def postorder(self):
         res = []
         def order(node):
@@ -52,12 +52,24 @@ class Tree:
         
 def solution(nodeinfo):
     answer = []
-    for i in range(len(nodeinfo)): nodeinfo[i] = [i+1] + nodeinfo[i]      
+    
+    #인덱스 정보 저장
+    for i in range(len(nodeinfo)):
+        nodeinfo[i] = [i+1] + nodeinfo[i]      
+        
+    #y값 기준으로 내림차순 정렬
     nodeinfo.sort(key=lambda x:(x[2]), reverse=True)
+    
+    #Tree와 함께 루트 노드까지 생성
     tree = Tree(nodeinfo[0][0], nodeinfo[0][1])
-    for i in nodeinfo[1:]: tree.insert(i[0], i[1])    
+    
+    #모든 노드를 트리에 삽입
+    for i in nodeinfo[1:]:
+        tree.insert(i[0], i[1])    
+    
     answer.append(tree.preorder())
     answer.append(tree.postorder())
+    
     return answer
 
 solution([[5,3],[11,5],[13,3],[3,5],[6,1],[1,3],[8,6],[7,2],[2,2]])
