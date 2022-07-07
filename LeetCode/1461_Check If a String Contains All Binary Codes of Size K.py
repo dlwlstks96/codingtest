de09911eee5cdd70d5ae0d066e892ff7d1ee8ced
Jinsan Lee <72801465+dlwlstks96@users.noreply.github.com>
2022-07-07 15:02:18 +0900

    LeetCode-1461-Check If a.. more efficient code

--- LeetCode/1461_Check If a String Contains All Binary Codes of Size K.py
+++ LeetCode/1461_Check If a String Contains All Binary Codes of Size K.py
@@ -21,3 +21,22 @@ class Solution:
             
         if count != 2**k:
             return False
+
+'''아래는 정답을 보고 내가 작성한 코드 / 시간 복잡도 6~8배 가량 줄어든다 / 딕셔너리 사용X
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:        
+        binary_set = set()
+        
+        #문자열 s 순회하며 탐색한 이진 코드인지 확인
+        count = 0
+        for i in range(len(s) - k + 1):
+            now_window = s[i:i+k] #문자열 s에서 현재 잘라낸 이진 코드
+            if now_window not in binary_set: #사전에 탐색하지 않은 이진 코드라면
+                binary_set.add(now_window) #추가
+                count += 1 #카운트
+            
+            if count == 2**k: #길이가 k인 모든 이진 코드를 찾았다면
+                return True
+            
+        return False
+'''
