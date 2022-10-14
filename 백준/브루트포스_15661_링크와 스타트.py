0155ef2164844e9254da211034f5c09d1d5630af
Jinsan Lee <72801465+dlwlstks96@users.noreply.github.com>
2022-10-14 13:44:58 +0900

    Update 브루트포스_15661_링크와 스타트.py

--- 백준/브루트포스_15661_링크와 스타트.py
+++ 백준/브루트포스_15661_링크와 스타트.py
@@ -1,28 +1,58 @@
+import itertools as it
+import math
+
 n = int(input())
-stats = [list(map(int, input().split())) for i in range(n)]
-visited = [0] * n
-ans = 99999
 
-def is_it():
-    global ans
-    start, link = 0, 0
-    for i in range(n):
-        for j in range(n):
-            if visited[i] and visited[j]:
-                start += stats[i][j]
-            elif not visited[i] and not visited[j]:
-                link += stats[i][j]
-    ans = min(ans, abs(start - link))
-    return
+board = []
 
-def resolve(iter):
-    if iter == n:
-        is_it()
-        return
-    visited[iter] = 1
-    resolve(iter + 1)
-    visited[iter] = 0
-    resolve(iter + 1)
-resolve(0)
+for _ in range(n):
+    tmp = list(map(int, input().split()))
+    board.append(tmp)
+    
+#print(board)
 
-print(ans)
+minVal = 9999999999 #두 팀의 능력치 차이 저장 / 최솟값 찾아야함
+
+for pickCount in range(1, math.floor(n/2)+1):
+    
+    if minVal == 0:
+        break
+    
+    pick = it.combinations(range(n), pickCount)
+    for p in pick:
+        p = list(p)
+        
+        linkSum = 0
+        starSum = 0
+        
+        link = []
+        star = []
+        
+        for linkMember in p: #link팀 멤버
+            link.append(linkMember)
+            
+        for starMember in range(n): #star팀 멤버
+            if starMember not in link:
+                star.append(starMember)
+        
+        for i in range(len(link)-1): #link팀 능력치 합산
+            for j in range(i+1, len(link)):
+                linkSum += board[link[i]][link[j]]
+                linkSum += board[link[j]][link[i]]
+                
+        for i in range(len(star)-1):
+            for j in range(i+1, len(star)):
+                starSum += board[star[i]][star[j]]
+                starSum += board[star[j]][star[i]]
+        
+        minVal = min(minVal, abs(linkSum - starSum))
+        
+        # print(link, linkSum)
+        # print(star, starSum)
+        # print(minVal)
+        # print('============', p)
+        
+        if minVal == 0:
+            break
+        
+print(minVal)
