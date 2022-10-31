1a20f1b52e2566e9a66ea6330439d914ffd066b1
Jinsan Lee <72801465+dlwlstks96@users.noreply.github.com>
2022-11-01 01:22:24 +0900

    Create 혼자 놀기의 달인.py

---
+++ 프로그래머스/혼자 놀기의 달인.py
@@ -0,0 +1,30 @@
+def solution(cards):
+    answer = 0
+    
+    visit = [0 for _ in range(len(cards))] #상자 방문 처리
+    saveCount = [] #각 그룹의 상자 갯수 저장
+    
+    for idx, nowCard in enumerate(cards):
+        
+        if visit[idx] == 0: #아직 안 열어본 상자라면
+            groupCount = 0
+            nextIdx = nowCard-1 #다음 열어볼 상자 인덱스
+            
+            while True:
+                if visit[nextIdx] == 0: #다음 상자가 안 열어본 상자라면
+                    groupCount += 1
+                    visit[nextIdx] = 1
+                    nextIdx = cards[nextIdx]-1
+                else: #다음 상자가 열어본 상자라면
+                    break
+                    
+            saveCount.append(groupCount) #현재 그룹의 상자 갯수 저장
+            
+    saveCount.sort(reverse=True) #오름차순 정렬
+    
+    if len(saveCount) > 1: #그룹이 2개 이상이라면 
+        answer = saveCount[0] * saveCount[1]
+    else:
+        answer = 0
+    
+    return answer
